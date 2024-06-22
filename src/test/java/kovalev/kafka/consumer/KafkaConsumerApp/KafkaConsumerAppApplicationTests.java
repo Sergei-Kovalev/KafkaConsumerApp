42e4bef020eeac28c5df6e452c922e917b7e4406
Sergey Kovalev <s.kovalev.jdev@gmail.com>
2024-06-22 18:25:28 +0300

    Initial commit

---
+++ src/test/java/kovalev/kafka/consumer/KafkaConsumerApp/KafkaConsumerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kovalev.kafka.consumer.KafkaConsumerApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
